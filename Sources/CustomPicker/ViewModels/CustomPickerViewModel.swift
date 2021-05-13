ad9a6e0d423a47a65f70bdc1d65326c56cf175e3
anikaseibezeder <anika.seib@gmail.com>
2021-05-13 15:25:41 +0200

    add custom picker view model

---
+++ Sources/CustomPicker/ViewModels/CustomPickerViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  CustomPickerViewModel.swift
+//  Decisly
+//
+//  Created by Anika Seibezeder on 13.05.21.
+//
+
+import Foundation
+
+class CustomPickerViewModel<SelectionValue>: NSObject where SelectionValue: Hashable {
+
+    // MARK: - Private properties
+
+    private var selection: Set<SelectionValue>
+    private var optionToString: (SelectionValue) -> String
+
+    // MARK: - Public properties
+
+    var selectedOptionsText: String {
+        switch selection.count {
+        case 0:
+            return ""
+        case 1:
+            return optionToString(selection[selection.startIndex])
+        default:
+            return "\(selection.count) Selected"
+        }
+    }
+
+    init(selection: Set<SelectionValue>,
+         optionToString: @escaping (SelectionValue) -> String) {
+        self.selection = selection
+        self.optionToString = optionToString
+    }
+}
+
