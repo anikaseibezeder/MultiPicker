be1eadb2da486976ba293826476ae40ba2fe28af
anikaseibezeder <anika.seib@gmail.com>
2021-05-14 15:47:59 +0200

    add string extension

---
+++ Sources/CustomPicker/Extensions/StringExtension.swift
@@ -0,0 +1,12 @@
+//
+//  StringExtension.swift
+//
+//
+//  Created by Anika Seibezeder on 13.05.21.
+//
+
+import Foundation
+
+extension String: Identifiable {
+    public var id: String { self }
+}
