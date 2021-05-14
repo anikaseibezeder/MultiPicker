014f385afd0003552faddd7ef08679de03d56ae3
anikaseibezeder <anika.seib@gmail.com>
2021-05-14 15:36:55 +0200

    add grid selection view

---
+++ Sources/CustomPicker/Views/GridSelectionView.swift
@@ -0,0 +1,100 @@
+//
+//  GridSelectionView.swift
+//  Decisly
+//
+//  Created by Anika Seibezeder on 13.05.21.
+//
+
+import SwiftUI
+
+struct GridSelectionView<SelectionValue>: View where SelectionValue: Identifiable & Hashable {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @Binding var selection: Set<SelectionValue>
+    var customPickerStyle: GridPickerStyle
+    var options: [String: [SelectionValue]]
+    var optionToString: (SelectionValue) -> String
+    
+    private var columns: [GridItem]
+    
+    init(selection: Binding<Set<SelectionValue>>,
+         customPickerStyle: GridPickerStyle,
+         options: [String: [SelectionValue]],
+         optionToString: @escaping (SelectionValue) -> String) {
+        self._selection = selection
+        self.customPickerStyle = customPickerStyle
+        self.options = options
+        self.optionToString = optionToString
+        self.columns = [GridItem](repeating: GridItem(.flexible()),
+                                  count: customPickerStyle.columns)
+    }
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(options.keys.sorted()) { section in
+                    Section(header: HStack {
+                        Text(section.capitalizeFirstLetter())
+                                .foregroundColor(Color(.label))
+                                .font(.title3)
+                                .fontWeight(.bold)
+
+                        Spacer()
+                    }) {
+                        ForEach(options[section] ?? []) { option in
+                            Button {
+                                if !customPickerStyle.allowsMultipleSelection {
+                                    selection.removeAll()
+                                }
+                                
+                                if let existingIndex = selection.firstIndex(where: { $0.id == option.id }) {
+                                    selection.remove(at: existingIndex)
+                                } else {
+                                    selection.insert(option)
+                                }
+                                
+                                if !customPickerStyle.allowsMultipleSelection {
+                                    presentationMode.wrappedValue.dismiss()
+                                }
+                            } label: {
+                                Text(optionToString(option))
+                                    .foregroundColor(Color(.label))
+                                    .font(
+                                        .title
+                                    )
+                                    .padding(8)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                            .fill(selection.contains { $0.id == option.id } ? Color.gray.opacity(0.2) : Color.gray.opacity(0))
+                                    )
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct GridSelectionView_Previews: PreviewProvider {
+    struct IdentifiableString: Identifiable, Hashable {
+        let string: String
+        var id: String { string }
+    }
+    
+    static let options = [
+        "Option 1",
+        "Option 2",
+        "Option 3"
+    ]
+
+    @State static var selectedOptions = Set<IdentifiableString>()
+    
+    static var previews: some View {
+        GridSelectionView(selection: $selectedOptions,
+                          customPickerStyle: GridPickerStyle(),
+                          options: ["": options.map { IdentifiableString(string: $0) }]) {
+            $0.string
+        }
+    }
+}
