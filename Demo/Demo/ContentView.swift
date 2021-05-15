aaf3fccbeef94d784e4340dad00cc3eda787a10b
anikaseibezeder <anika.seib@gmail.com>
2021-05-15 11:21:40 +0200

    add using of MultiPicker in demo app

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -6,11 +6,45 @@
 //
 
 import SwiftUI
+import CustomPicker
 
 struct ContentView: View {
+    @State private var selectedOptionsDefaultStyle = Set<String>()
+    @State private var selectedOptionsGridStyle = Set<String>()
+    
+    @State private var allowsMultipleSelectionDefaultStyle = false
+    @State private var allowsMultipleSelectionGridStyle = false
+    
+    private let optionsDefaultStyle = ["Option 1", "Option 2"]
+    private let optionsGridStyle = ["😃", "😄", "😁", "😆", "😅"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("DefaultMultiPickerStyle")) {
+                    Toggle("Allows multiple selection", isOn: $allowsMultipleSelectionDefaultStyle)
+                    
+                    CustomPicker("Options",
+                                 selection: $selectedOptionsDefaultStyle,
+                                 options: optionsDefaultStyle) {
+                        $0
+                    }
+                    .customPickerStyle(DefaultCustomPickerStyle(allowsMultipleSelection: allowsMultipleSelectionDefaultStyle))
+                }
+                
+                Section(header: Text("GridPickerStyle")) {
+                    Toggle("Allows multiple selection", isOn: $allowsMultipleSelectionGridStyle)
+                    
+                    CustomPicker("Options",
+                                 selection: $selectedOptionsGridStyle,
+                                 options: optionsGridStyle) {
+                        $0
+                    }
+                    .customPickerStyle(GridPickerStyle(columns: 5, allowsMultipleSelection: allowsMultipleSelectionGridStyle))
+                }
+            }
+            .navigationTitle("MultiPicker Demo")
+        }
     }
 }
 
