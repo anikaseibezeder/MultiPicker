3a8a3ef154ae4350abf077090fd77fa48461de26
anikaseibezeder <anika.seib@gmail.com>
2021-05-15 11:07:15 +0200

    add basic demo app

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Anika Seibezeder on 15.05.21.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
